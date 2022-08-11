606e4820504f5b87d7d2dc49f3fd63cc9de5522c
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2022-08-11 08:40:13 -0500

    Update context-menu.js

--- context-menu.js
+++ context-menu.js
@@ -2,7 +2,7 @@
 document.createElement("context-menu");
 
 function menu(id) {
-var element = document.getElementById();
+var element = document.getElementById(id);
 window.open("#"+id, "_self"); //to get to the menu
 element.style.display = "block";
 var tag = document.createElement("h1");
